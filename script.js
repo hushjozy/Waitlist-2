9cd9e6bd016a5daa3cf618dc6eef46abaa75160f
Joseph <hushjoezzybad@gmail.com>
2023-01-30 16:32:55 +0100

    waitlist go test

---
+++ script.js
@@ -0,0 +1,25 @@
+let countDownDate = new Date("March 10, 2023 00:00:00").getTime();
+let x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  let countdown = document.getElementById("countdown-timer");
+  if (distance <= 3600000) {
+    countdown.classList.add("smaller");
+  } else {
+    countdown.classList.remove("smaller");
+  }
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown-timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
